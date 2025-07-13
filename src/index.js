69ca533c485ded3a4c7719e2ddb5c4ce2886760b
thuanAlex30 <thuandh30@gmail.com>
2025-07-13 18:47:10 +0700

    update chatbox-portfolio

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '@/contexts/AuthContext';
 import App from './App';
 import { StyleProvider } from '@ant-design/cssinjs';
+import { SocketProvider } from "@/contexts/SocketContext";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ConfigProvider
@@ -18,8 +19,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <BrowserRouter>
         <AuthProvider>
         <StyleProvider hashPriority="high">
-      <App />
-    </StyleProvider>
+          <SocketProvider>
+            <App />
+          </SocketProvider>
+        </StyleProvider>
         </AuthProvider>
       </BrowserRouter>
     </ConfigProvider>
